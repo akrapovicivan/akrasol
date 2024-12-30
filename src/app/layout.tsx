4de0b305d579142d22daefdeabe8073e6c462772
Ivan Akrapović <iakrap00@fesb.hr>
2024-12-30 12:02:33 +0100

    home page design implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import Header from './components/Header/Header';
 import './globals.css';
+
+config.autoAddCss = false;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt, faSackDollar, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import Section from './components/Header/Section'
+
 export default function HomePage() {
     return (
         <div>
-            <h1>Welcome to Solar Panel Installer</h1>
-            <p>Your trusted partner in renewable energy solutions!</p>
+            <div className="w-full h-[582px] bg-cover bg-center" 
+                style={{ backgroundImage: 'linear-gradient(0deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%), url(/images/panels-hero.png)' }}>
+                <div className="ml-20 w-[461px] h-full bg-[#212121]/40 text-white flex flex-col justify-end p-10" >
+                    <span className="text-5xl mb-8">Title</span>
+                    <p className="text-base mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+                    Fames ipsum pulvinar dictumst adipiscing et, augue phasellus id sapien.</p>
+                </div>
+            </div>
+            <div className="py-8 flex flex-row justify-center gap-x-20">
+                <div className="flex flex-col w-[120px]">
+                    <FontAwesomeIcon icon={faBolt} size="2x" className="text-[#616161]" />
+                    <span className='text-sm text-[#616161] mt-3 text-center'>Power your home</span>
+                </div>
+                <div className="flex flex-col w-[120px]">
+                    <FontAwesomeIcon icon={faSackDollar} size="2x" className="text-[#616161]" />
+                    <span className='text-sm text-[#616161] mt-3 text-center'>Save money</span>
+                </div>
+                <div className="flex flex-col w-[120px]">
+                    <FontAwesomeIcon icon={faLeaf} size="2x" className="text-[#616161]" />
+                    <span className='text-sm text-[#616161] mt-3 text-center'>No emmisions</span>
+                </div>
+            </div>
+            <div className='px-20'>
+                <Section image='/images/setup.png'
+                         title='Section title'
+                         description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                         Fames ipsum pulvinar dictumst adipiscing et, augue phasellus id sapien'
+                         direction='flex-row'>
+                </Section>
+                <Section outerBackgroundImage='/images/panel-closeup.jpg'
+                         title='Section title'
+                         description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                         Fames ipsum pulvinar dictumst adipiscing et, augue phasellus id sapien'
+                         direction='flex-row-reverse'
+                         innerBackgroundColor='#212121'>
+                </Section>
+                <Section image='/images/layered-panel.png'
+                         title='Section title'
+                         description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                         Fames ipsum pulvinar dictumst adipiscing et, augue phasellus id sapien'
+                         direction='flex-row-reverse'>
+                </Section>
+            </div>
         </div>
     );
 }
