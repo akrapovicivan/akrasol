042025188932f7826572c61dd0963a7d7376161a
dmisadin <dominik.misadin@gmail.com>
2025-01-18 16:52:48 +0100

    Minor improvements on responsivity for homepage

--- src/app/components/Header/Logo.tsx
+++ src/app/components/Header/Logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Logo({ className = "", width = 100, height = 100 }) {
     return (
-        <div className={`inline-flex ${className}`}>
-        <Image
-            src="/svg/logo.svg"
-            alt="Logo"
-            width={width}
-            height={height}
-        />
-    </div>
+        <div className={`min-w-16 inline-flex ${className}`}>
+            <Image
+                src="/svg/logo.svg"
+                alt="Logo"
+                width={width}
+                height={height}
+            />
+        </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import Section from './components/Header/Section'
 export default function HomePage() {
     return (
         <div>
-            <div className="w-full h-[582px] bg-cover bg-center px-48" 
+            <div className="w-full h-[582px] bg-cover bg-center px-48 max-[888px]:px-16" 
                 style={{ backgroundImage: 'linear-gradient(0deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%), url(/images/panels-hero.png)' }}>
-                <div className="min-[888px]:ml-20 min-[888px]:w-[461px] h-full bg-[#212121]/40 text-white flex flex-col justify-end p-10" >
+                <div className="min-[888px]:w-[461px] h-full bg-[#212121]/40 text-white flex flex-col justify-end p-10" >
                     <span className="text-5xl mb-8">Title</span>
                     <p className="text-base mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
                     Fames ipsum pulvinar dictumst adipiscing et, augue phasellus id sapien.</p>
                 </div>
             </div>
-            <div className="p-8 px-48 flex flex-row justify-center gap-x-20">
+            <div className="p-8 px-48 max-[888px]:px-16 flex flex-row justify-center gap-x-20">
                 <div className="flex flex-col w-[120px]">
                     <FontAwesomeIcon icon={faBolt} size="2x" className="text-[#616161]" />
                     <span className='text-sm text-[#616161] mt-3 text-center'>Power your home</span>
@@ -27,7 +27,7 @@ export default function HomePage() {
                     <span className='text-sm text-[#616161] mt-3 text-center'>No emmisions</span>
                 </div>
             </div>
-            <div className='px-48'>
+            <div className='px-48 max-[888px]:px-16'>
                 <Section image='/images/setup.png'
                          title='Section title'
                          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
