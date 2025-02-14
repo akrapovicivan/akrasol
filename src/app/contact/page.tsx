7651d37e4065d44d8753eedbdf81dd2136446c08
dmisadin <dominik.misadin@gmail.com>
2025-02-14 20:26:02 +0100

    Adjust input padding and border color, fix responsivity on Contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,89 +48,91 @@ export default function ContactForm() {
   };
 
   return (
-    <div className="flex flex-col mt-12 w-[90%] mx-auto md:justify-center md:flex-row bg-gray-50 rounded-md shadow-md p-8">
-      <div className="md:w-2/5 md:flex md:flex-col md:justify-evenly p-6 mb-8 space-y-6">
-        <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="text-blue-600 text-2xl" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Our Main Office</h3>
-            <p className="text-gray-500 text-sm">Podmeje 23, Kaštela</p>
-          </div>
+    <div className="w-full px-48 max-[888px]:px-16">
+      <div className="flex flex-col mt-12 mx-auto md:justify-center md:flex-row bg-gray-50 rounded-md shadow-md md:py-8 p-8">
+        <div className="md:w-2/5 md:flex md:flex-col md:justify-evenly p-6 mb-8 space-y-6">
+            <div className="flex items-center space-x-4">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="text-blue-600 text-2xl" />
+            <div>
+                <h3 className="text-xl font-semibold text-gray-700">Our Main Office</h3>
+                <p className="text-gray-500 text-sm">Podmeje 23, Kaštela</p>
+            </div>
+            </div>
+            <div className="flex items-center space-x-4">
+            <FontAwesomeIcon icon={faPhone} className="text-blue-600 text-2xl" />
+            <div>
+                <h3 className="text-xl font-semibold text-gray-700">Phone Number</h3>
+                <p className="text-gray-500 text-sm">091-110-9101</p>
+            </div>
+            </div>
+            <div className="flex items-center space-x-4">
+            <FontAwesomeIcon icon={faEnvelope} className="text-blue-600 text-2xl" />
+            <div>
+                <h3 className="text-xl font-semibold text-gray-700">Email</h3>
+                <p className="text-gray-500 text-sm">akrasol@info.com</p>
+            </div>
+            </div>
+            <div className="flex items-center space-x-4">
+            <FontAwesomeIcon icon={faClock} className="text-blue-600 text-2xl" />
+            <div>
+                <h3 className="text-xl font-semibold text-gray-700">Business Hours</h3>
+                <p className="text-gray-500 text-sm">Mon - Fri: 8:00 AM - 6:00 PM</p>
+            </div>
+            </div>
         </div>
-        <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faPhone} className="text-blue-600 text-2xl" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Phone Number</h3>
-            <p className="text-gray-500 text-sm">091-110-9101</p>
-          </div>
-        </div>
-        <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faEnvelope} className="text-blue-600 text-2xl" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Email</h3>
-            <p className="text-gray-500 text-sm">akrasol@info.com</p>
-          </div>
-        </div>
-        <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faClock} className="text-blue-600 text-2xl" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Business Hours</h3>
-            <p className="text-gray-500 text-sm">Mon - Fri: 8:00 AM - 6:00 PM</p>
-          </div>
-        </div>
-      </div>
 
-      <div className="md:w-2/5 bg-white p-6 rounded-md shadow-md">
-        <h2 className="text-xl font-semibold text-gray-700 mb-4">Contact Us</h2>
-        <p className="text-gray-500 text-sm mb-3">Fields marked <span className="text-blue-600">*</span> are required.</p>
-        <form onSubmit={handleSubmit} className="space-y-4">
-            <label className="block mb-3 w-full  md:pr-5">
-                <span className="text-gray-700">Name<span className="text-blue-600 text-xl">*</span></span>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Enter your Name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    className="mt-3 px-2 block w-full h-[30px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                    required
-                />
-            </label>
-            <label className="block mb-3 w-full  md:pr-5">
-                <span className="text-gray-700">Email<span className="text-blue-600 text-xl">*</span></span>
-                <input
-                    type="text"
-                    name="email"
-                    placeholder="Enter a valid email address"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="mt-3 px-2 block w-full h-[30px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                    required
-                />
-            </label>
-          
-            <label className="block mb-3 w-full  md:pr-5">
-                <span className="text-gray-700">Message<span className="text-blue-600 text-xl">*</span></span>
-                <textarea
-                    name="message"
-                    placeholder="Your Message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    className="mt-3 px-2 block w-full h-[100px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                    required
-                />
-            </label>
+        <div className="md:w-2/5 bg-white p-6 rounded-md shadow-md">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">Contact Us</h2>
+            <p className="text-gray-500 text-sm mb-3">Fields marked <span className="text-blue-600">*</span> are required.</p>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <label className="block mb-3 w-full  md:pr-5">
+                    <span className="text-gray-700">Name<span className="text-blue-600 text-xl">*</span></span>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your Name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        className="mt-3 px-2 block w-full h-[30px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                        required
+                    />
+                </label>
+                <label className="block mb-3 w-full  md:pr-5">
+                    <span className="text-gray-700">Email<span className="text-blue-600 text-xl">*</span></span>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Enter a valid email address"
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="mt-3 px-2 block w-full h-[30px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                        required
+                    />
+                </label>
+            
+                <label className="block mb-3 w-full  md:pr-5">
+                    <span className="text-gray-700">Message<span className="text-blue-600 text-xl">*</span></span>
+                    <textarea
+                        name="message"
+                        placeholder="Your Message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        className="mt-3 px-2 block w-full h-[100px] rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                        required
+                    />
+                </label>
 
-          
-            <button
-                type="submit"
-                onClick={sendInquiry}
-                disabled={!formData.message || !formData.email || !formData.email}
-                className="px-4 mt-5 block py-2 ml-auto bg-blue-600 text-white rounded-md enabled:hover:bg-blue-700 disabled:opacity-80 disabled:pointer-disabled disabled:cursor-not-allowed"
-            >
-                Submit
-            </button>
-        </form>
+            
+                <button
+                    type="submit"
+                    onClick={sendInquiry}
+                    disabled={!formData.message || !formData.email || !formData.email}
+                    className="px-4 mt-5 block py-2 ml-auto bg-blue-600 text-white rounded-md enabled:hover:bg-blue-700 disabled:opacity-40 disabled:pointer-disabled disabled:cursor-not-allowed"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
       </div>
     </div>
   );
