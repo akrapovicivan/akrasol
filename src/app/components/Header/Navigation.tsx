d755e88ed706aaf487fa91acc3cb4f1ad1dea346
dmisadin <dominik.misadin@gmail.com>
2025-02-16 13:04:04 +0100

    Apply Hick's law to sidebar route grouping

--- src/app/components/Header/Navigation.tsx
+++ src/app/components/Header/Navigation.tsx
@@ -70,7 +70,7 @@ export default function Navigation({ isSidebar = false,  closeSidebar }: { isSid
 
     return (
         <nav className={`h-full ${isSidebar ? "mt-4" : "text-black"}`}>
-            <ul className={`h-full ${isSidebar ? "flex flex-col gap-y-8" : "flex"}`}>
+            <ul className={`h-full ${isSidebar ? "flex flex-col" : "flex"}`}>
                 {navigation.map((item) => {
                     let isActive = false;
                     if (item.subroutes?.length)
@@ -79,18 +79,18 @@ export default function Navigation({ isSidebar = false,  closeSidebar }: { isSid
                         isActive = pathname.startsWith(item.href)
                     
                     return (
-                        <li key={item.name} className={`${!isSidebar ? "h-full" : ""} relative group whitespace-nowrap`}>
+                        <li key={item.name} className={`${!isSidebar ? "h-full" : "py-4"} relative group whitespace-nowrap hover:bg-blue-600 hover:text-white ${
+                                    isActive
+                                        ? "bg-blue-600 text-white"
+                                        : ""}`}>
                             <Link
                                 href={item.href}
                                 onClick={handleLinkClick}
                                 className={`flex items-center ${styles['nav-route']} block ${
                                     isSidebar
-                                        ? "h-16 px-6 text-lg font-bold hover:bg-blue-600 hover:text-white"
-                                        : "h-full hover:bg-blue-600 hover:text-white transition duration-200 p-8"
-                                } ${
-                                    isActive
-                                        ? "bg-blue-600 text-white font-bold"
-                                        : ""}`}
+                                        ? "h-16 px-6 text-lg font-bold"
+                                        : "h-full transition duration-200 p-8"
+                                }`}
                             >
                                 {item.name}
                             </Link>
@@ -99,25 +99,31 @@ export default function Navigation({ isSidebar = false,  closeSidebar }: { isSid
                                 <ul
                                     className={`${styles['nav-subroute']} z-50 ${
                                         isSidebar
-                                            ? "px-8 space-y-2"
+                                            ? ""
                                             : "absolute left-0 mt-2 min-w-64 bg-white shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200"
                                     }`}
                                 >
-                                    {item.subroutes.map((subitem) => (
-                                        <li key={subitem.name}>
-                                            <Link
-                                                href={subitem.href}
-                                                onClick={handleLinkClick}
-                                                className={`block ${
-                                                    isSidebar
-                                                        ? "text-sm hover:underline"
-                                                        : "p-4 hover:text-blue-600 transition duration-200"
-                                                }`}
-                                            >
-                                                {subitem.name}
-                                            </Link>
-                                        </li>
-                                    ))}
+                                    {item.subroutes.map((subitem) => 
+                                    {
+                                        const isActive = pathname.startsWith(subitem.href);
+                                        return (
+                                            <li key={subitem.name}>
+                                                <Link
+                                                    href={subitem.href}
+                                                    onClick={handleLinkClick}
+                                                    className={`block px-8 ${
+                                                        isSidebar
+                                                            ? "py-2 text-sm hover:bg-black/50"
+                                                            : "p-4 hover:text-blue-600 text-black transition duration-200"
+                                                    }  ${
+                                                        isActive
+                                                            ? "font-bold"
+                                                            : ""}`
+                                                    }>
+                                                    {subitem.name}
+                                                </Link>
+                                            </li>
+                                    )})}
                                 </ul>
                             )}
                         </li>
