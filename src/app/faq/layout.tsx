b48c4906fdeb23098b22118846043f7ab694c6d7
dmisadin <dominik.misadin@gmail.com>
2025-02-16 19:59:22 +0100

    Add FAQ

--- src/app/faq/layout.tsx
+++ src/app/faq/layout.tsx
@@ -1,7 +1,6 @@
 export default function FaqLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <h2>FAQ</h2>
             {children}
         </div>
     );
