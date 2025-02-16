e73a8031f91953e40451ad67509e005f814394dc
dmisadin <dominik.misadin@gmail.com>
2025-02-16 13:20:23 +0100

    Add EndingOn to offers with formatting

--- src/app/offers/[offerId]/page.tsx
+++ src/app/offers/[offerId]/page.tsx
@@ -24,6 +24,12 @@ export default async function OfferDetailsPage({ params }: { params: { offerId: 
         return notFound();
     }
 
+    const endingOn = new Date(offer.attributes.EndingOn);
+    const formatter = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long", // "Thursday, March 20, 2025"
+    });
+
+
     return (
         /*
         <div>
@@ -35,13 +41,14 @@ export default async function OfferDetailsPage({ params }: { params: { offerId: 
         <section className="bg-white px-4 py-8 antialiased dark:bg-gray-900 md:py-16">
             <div className="w-full grid rounded-lg bg-blue-50 p-4 dark:bg-blue-800 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16 gap-8">
                 <div className="lg:col-span-5 lg:mt-0">
-                    <img className="mb-4 h-56 w-56 sm:h-96 sm:w-96 md:h-full md:w-full rounded-xl max-h-[768px] max-w-[768px]" src={"/images/offers/" + params.offerId + ".webp"} alt="peripherals" />
+                    <img className="mb-4 h-56 w-56 sm:h-96 sm:w-96 md:h-full md:w-full rounded-xl aspect-square" src={"/images/offers/" + params.offerId + ".webp"} alt="peripherals" />
                 </div>
-                <div className="me-auto place-self-center lg:col-span-7">
+                <div className="h-full me-auto place-self-center lg:col-span-7 flex flex-col justify-between">
                     <h1 className="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">
                         {offer.attributes.Name}
                     </h1>
-                    <p className="mb-6 text-gray-500 dark:text-gray-400">{offer.attributes.Description}</p>
+                    <p className="mb-6 text-gray-500 dark:text-gray-400" style={{lineHeight:" 2rem"}}>{offer.attributes.Description}</p>
+                    <p className="mb-6 text-gray-500 dark:text-gray-400">Offer is ending on: { formatter.format(endingOn) }</p>
                     <button className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"> Order now </button>
                 </div>
             </div>
