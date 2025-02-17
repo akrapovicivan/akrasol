7a6281709a2a9599926f5cf625526e91fe6a7e8f
dmisadin <dominik.misadin@gmail.com>
2025-02-17 15:23:32 +0100

    Fix padding on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center mt-8">
+        <div className="flex flex-col items-center justify-center my-16 lg:px-48 sm:px-16 px-4 my-auto relative text-center">
             <Image src="/svg/404.svg" width="400" height="400" alt="404"></Image>
             <h1 className="text-4xl font-bold mt-8">Oops! - Page Not Found</h1>
             <p className="text-lg mt-4">Sorry, the page you are looking for does not exist.</p>
-            <Link href="/" className="h-8 px-16 py-8 mt-8 flex items-center text-lg text-blue-600 hover:text-blue-700 hover:font-bold border border-blue-600 text-blue-600 hover:bg-blue-100 rounded-md">Go back to home</Link>
+            <Link href="/" className="sm:px-16 px-4 sm:py-4 py-2 mt-8 flex items-center text-lg text-blue-600 hover:text-blue-700 hover:font-bold border border-blue-600 text-blue-600 hover:bg-blue-100 rounded-md">Go back to home</Link>
         </div>
     );
 }
