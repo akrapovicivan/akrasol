aff966f6693060057fcda0d1394b270aa7fb852e
Ivan Akrapović <iakrap00@fesb.hr>
2025-05-19 20:49:10 +0200

    zatvori fix.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 export default function Header() {
 
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    
+
     useEffect(() => {
     }, []);
 
@@ -36,34 +36,34 @@ export default function Header() {
                     </svg>
                 </button>
 
-                    <div className="mx-auto min-[888px]:mx-0 pr-5">
-                        <Logo width={80} height={48} />
-                    </div>
+                <div className="mx-auto min-[888px]:mx-0 pr-5">
+                    <Logo width={80} height={48} />
+                </div>
 
-                    <div className={`hidden min-[888px]:block ${!isSidebarOpen ? "h-full" : ""}`}>
-                        <Navigation />
-                    </div>
+                <div className={`hidden min-[888px]:block ${!isSidebarOpen ? "h-full" : ""}`}>
+                    <Navigation />
+                </div>
             </div>
             {isSidebarOpen && (
-            <div className={`fixed inset-0 z-40 min-[888px]:hidden ${isSidebarOpen ? "block" : "hidden"}`} >
-                <div className={`fixed top-0 left-0 w-64 bg-white shadow-md z-50 flex flex-col ${isSidebarOpen ? "h-full" : ""}`}>
-                    <button
-                        className="p-4 self-end flex gap-2"
-                        onClick={closeSidebar}
-                        aria-label="Close Sidebar"
-                    >
-                        Zatvori
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+                <div className={`fixed inset-0 z-40 min-[888px]:hidden ${isSidebarOpen ? "block force-dark" : "hidden"}`} >
+                    <div className={`fixed top-0 left-0 w-64 bg-white shadow-md z-50 flex flex-col ${isSidebarOpen ? "h-full" : ""}`}>
+                        <button
+                            className="p-4 self-end flex gap-2 force-dark"
+                            onClick={closeSidebar}
+                            aria-label="Close Sidebar"
+                        >
+                            Zatvori
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
 
-                    <Navigation isSidebar closeSidebar={closeSidebar} />
-                </div>
-                <div className="fixed bg-gray-800 bg-opacity-50 w-full h-full" onClick={closeSidebar}>
+                        <Navigation isSidebar closeSidebar={closeSidebar} />
+                    </div>
+                    <div className="fixed bg-gray-800 bg-opacity-50 w-full h-full" onClick={closeSidebar}>
 
+                    </div>
                 </div>
-            </div>
             )}
         </header>
     );
