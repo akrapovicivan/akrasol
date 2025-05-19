21c5566a23aa035aa760ee67b1fa29d14c64ed9c
Ivan Akrapović <iakrap00@fesb.hr>
2025-05-19 20:51:10 +0200

    euro instead of dollar

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -25,7 +25,7 @@ export default function FaqPage() {
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                                 Koliko koštaju solarni paneli?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">Cijena ovisi o veličini sustava, lokaciji i dostupnim poticajima. U prosjeku, kućni sustav može koštati između 10.000 i 30.000 dolara prije poticaja.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Cijena ovisi o veličini sustava, lokaciji i dostupnim poticajima. U prosjeku, kućni sustav može koštati između 10.000 i 30.000 eura prije poticaja.</p>
                         </div>
 
                         <div className="mb-10">
