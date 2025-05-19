78e54412a13bb8e524f586ddf805416a58599a88
Ivan Akrapović <iakrap00@fesb.hr>
2025-05-19 20:12:55 +0200

    coonfig.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// next.config.js
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
@@ -6,4 +6,4 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
